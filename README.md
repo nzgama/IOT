ff6368aa8358a08d4b9da029951f6590ff8a0350
Gamal <gamalmengarelli@gmail.com>
2023-08-10 22:14:18 -0300

    dth22-btn

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-# Ejercicio 3
-## Escribir un programa para el esp32 basado en el circuito utilizado en el capítulo 3, que muestre en la terminal, cada 20 segundos, las mediciones de humedad y temperatura del sensor DHT22.
+# Ejercicio 4
+## Escribir un programa para el esp32 basado en el circuito utilizado en el capítulo 3, que al accionar el pulsador, registra las mediciones de humedad y temperatura del sensor DHT22. Al pulsar una segunda vez vuelve a medir y muestra los promedios de las dos mediciones de humedad y temperatura.
--- main.py
+++ main.py
@@ -1,26 +1,48 @@
 # Germán Andrés Xander 2023
 
-from machine import Pin
+from machine import Pin, Timer
 import dht
 import time
 import json
 from collections import OrderedDict
 
+sw = Pin(23, Pin.IN)
+led = Pin(2, Pin.OUT)
 d = dht.DHT22(Pin(25))
-print("Sensor temperatura & humedad ...")
-contador=0
+print("esperand pulsador")
+estado = False
+temperatura = 0
+humedad = -1
+
+def alternar(pin):
+    global contador, estado, temperatura, humedad 
+    if sw.value():
+        if not estado:
+            try:
+                d.measure()
+                if humedad < 0:
+                    temperatura = d.temperature()
+                    humedad = d.humidity()
+                    print("1er medicion")
+                else:
+                    temperatura = (temperatura + d.temperature())/2
+                    humedad = (humedad + d.humidity())/2
+                    print("2da medicion")
+                    datos = json.dumps(OrderedDict([
+                        ('Temperatura',temperatura),
+                        ('Humedad',humedad)
+                    ]))
+                    print(datos)
+                    humedad = -1
+            except OSError as e:
+                print("sin sensor")
+
+        estado = True
+    else:
+        estado = False
+
+timer1 = Timer(1)
+timer1.init(period=50, mode=Timer.PERIODIC, callback=alternar)
 
 while True:
-    try:
-        d.measure()
-        temperatura=d.temperature()
-        humedad=d.humidity()
-        datos=json.dumps(OrderedDict([
-            ('temperatura',temperatura),
-            ('humedad',humedad)
-        ]))
-        print(datos)
-    except OSError as e:
-        print("sin sensor")
-
-    time.sleep(20)
+    time.sleep(1)
