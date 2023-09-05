45453f5773221d1195b8976b74b7853e57cc7266
Gamal <gamalmengarelli@gmail.com>
2023-09-04 23:57:22 -0300

    dth22-btn

--- boot.py
+++ boot.py
@@ -1,1 +1,14 @@
-# boot.py -- run on boot-up
+from settings import SSID, PASS_WLAN
+
+def do_connect():
+    import network
+    wlan = network.WLAN(network.STA_IF)
+    wlan.active(True)
+    if not wlan.isconnected():
+        print('connecting to network...')
+        wlan.connect(SSID, PASS_WLAN)
+        while not wlan.isconnected():
+            pass
+    print('network config:', wlan.ifconfig())
+
+do_connect()
--- main.py
+++ main.py
@@ -5,44 +5,49 @@ import dht
 import time
 import json
 from collections import OrderedDict
+import urequests
+from settings import TOKEN, CHATID
 
 sw = Pin(23, Pin.IN)
 led = Pin(2, Pin.OUT)
 d = dht.DHT22(Pin(25))
+
 print("esperand pulsador")
-estado = False
-temperatura = 0
-humedad = -1
+contador=0
+estado=False
 
 def alternar(pin):
-    global contador, estado, temperatura, humedad 
+    global contador, estado
     if sw.value():
         if not estado:
+            contador+=1
+            print(contador)
+            led.value(not led.value())
             try:
-                d.measure()
-                if humedad < 0:
-                    temperatura = d.temperature()
-                    humedad = d.humidity()
-                    print("1er medicion")
-                else:
-                    temperatura = (temperatura + d.temperature())/2
-                    humedad = (humedad + d.humidity())/2
-                    print("2da medicion")
-                    datos = json.dumps(OrderedDict([
-                        ('Temperatura',temperatura),
-                        ('Humedad',humedad)
-                    ]))
-                    print(datos)
-                    humedad = -1
-            except OSError as e:
-                print("sin sensor")
-
-        estado = True
-    else:
-        estado = False
+                data = {'chat_id': CHATID, 'text': datos}
+                response = urequests.post("https://api.telegram.org/bot" + TOKEN + '/sendMessage', json=data)
+                # print(response.text)
+                response.close()
+                print("envio correcto a telegram")
+            except:
+                print("fallo en el envio a telegram")
+            estado = True
+        else:
+            estado = False
 
 timer1 = Timer(1)
 timer1.init(period=50, mode=Timer.PERIODIC, callback=alternar)
 
 while True:
-    time.sleep(1)
+    try:
+        d.measure()
+        temperatura=d.temperature()
+        humedad=d.humidity()
+        datos=json.dumps(OrderedDict([
+            ('temperatura',temperatura),
+            ('humedad',humedad)
+        ]))
+        print(datos)
+    except OSError as e:
+        print("sin sensor")
+    time.sleep(5)
