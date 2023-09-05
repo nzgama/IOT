1456ccc4e2e321ecc762e10644eca9327f31399b
Gamal <gamalmengarelli@gmail.com>
2023-09-05 00:02:41 -0300

    mqtt

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-# Ejercicio 4
-## Escribir un programa para el esp32 basado en el circuito utilizado en el capítulo 3, que al accionar el pulsador, registra las mediciones de humedad y temperatura del sensor DHT22. Al pulsar una segunda vez vuelve a medir y muestra los promedios de las dos mediciones de humedad y temperatura.
+# Ejercicio 5
+## Escribir un programa para el esp32, basado en el circuito utilizado en el capítulo 9, que publique un solo mensaje, a un broker mqtt, cuando la temperatura supera un valor establecido como "temperatura superior". El sistema no volverá a publicar mensajes hasta no haber estado por debajo de un límite inferior y volver a superar la temperatura superior.
--- main.py
+++ main.py
@@ -1,53 +1,57 @@
 # Germán Andrés Xander 2023
 
-from machine import Pin, Timer
+from machine import Pin, Timer, unique_id
 import dht
 import time
 import json
+import ubinascii
 from collections import OrderedDict
-import urequests
-from settings import TOKEN, CHATID
+from settings import SERVIDOR_MQTT
+from umqtt.robust import MQTTClient
 
-sw = Pin(23, Pin.IN)
-led = Pin(2, Pin.OUT)
+CLIENT_ID = ubinascii.hexlify(unique_id()).decode('utf-8')
+
+mqtt = MQTTClient(CLIENT_ID, SERVIDOR_MQTT,
+                  port=8883, keepalive=10, ssl=True)
+
 d = dht.DHT22(Pin(25))
-
-print("esperand pulsador")
-contador=0
-estado=False
-
-def alternar(pin):
-    global contador, estado
-    if sw.value():
-        if not estado:
-            contador+=1
-            print(contador)
-            led.value(not led.value())
-            try:
-                data = {'chat_id': CHATID, 'text': datos}
-                response = urequests.post("https://api.telegram.org/bot" + TOKEN + '/sendMessage', json=data)
-                # print(response.text)
-                response.close()
-                print("envio correcto a telegram")
-            except:
-                print("fallo en el envio a telegram")
-            estado = True
-        else:
-            estado = False
-
-timer1 = Timer(1)
-timer1.init(period=50, mode=Timer.PERIODIC, callback=alternar)
+contador = 0
+tempMax = False
+tempMin = False
+flag = False
 
 while True:
     try:
         d.measure()
-        temperatura=d.temperature()
-        humedad=d.humidity()
-        datos=json.dumps(OrderedDict([
+        temperatura = d.temperature()
+        humedad = d.humidity()
+        datos = json.dumps(OrderedDict([
             ('temperatura',temperatura),
             ('humedad',humedad)
         ]))
         print(datos)
+
+        if temperatura > 25:
+            tempMax = True
+
+        if temperatura < 20:
+            tempMin = True
+
+        if tempMax & tempMin & (temperatura > 25):
+            flag = True
+        
+        print("temperatura max", tempMax)
+        print("temperatura min", tempMin)
+
+        if flag:
+            print("publicando temperatura")
+            mqtt.connect()
+            mqtt.publish(f"ap/{CLIENT_ID}",datos)
+            mqtt.disconnect()     
+            flag = False
+            tempMax = False
+            tempMin = False
+
     except OSError as e:
         print("sin sensor")
     time.sleep(5)
